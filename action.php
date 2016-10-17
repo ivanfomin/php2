42f88d9b108d43eada0d5c9b06ff260efd04ede0
Ivan Fomin <la-verne@mail.ru>
2016-10-17 11:38:13 +0300

    second home_workgit status !

--- action.php
+++ action.php
@@ -2,32 +2,24 @@
 
 require_once __DIR__ . '/autoload.php';
 
-//var_dump($_POST);
 
 $id = $_POST['id'];
 $title = $_POST['title'];
 $content = $_POST['content'];
 
-$articles = \App\Model\Article::findAll();
-
 if (isset($_POST['update'])) {
 
-    foreach ($articles as $article) {
-        if ($id === $article->id) {
-            $article->title = $title;
-            $article->content = $content;
-            $article->save();
-        }
-    }
+    $article = \App\Model\Article::findById($id);
+
+    $article->title = $title;
+    $article->content = $content;
+    $article->save();
 
 } else if (isset($_POST['delete'])) {
-    foreach ($articles as $article) {
-        if ($id === $article->id) {
-            $article->title = $title;
-            $article->content = $content;
-            $article->delete();
-        }
-    }
+    $article = \App\Model\Article::findById($id);
+
+    $article->delete();
+
 } else if (isset($_POST['insert'])) {
     $article = new \App\Model\Article();
     $article->title = $title;
